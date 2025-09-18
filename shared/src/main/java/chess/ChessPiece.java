6b136d63e2b223ce8902b890c3d2a12738d490e5
Johnathan Goodlad <thejagoodlad@gmail.com>
2025-09-17 19:31:02 -0600

    Added moves for Rook, and overrode equals and hashcode functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -16,11 +17,42 @@ public class ChessPiece {
     private final PieceType type;
 
     private static final int[][] BISHOP_DIRECTIONS = {
-        {1,1}, //Up and right
-        {1,-1}, //Up and left
-        {-1,-1}, //Down and left
-        {-1,1} //Down and right
+            {1,1}, //Up and right
+            {1,-1}, //Up and left
+            {-1,-1}, //Down and left
+            {-1,1} //Down and right
     };
+    private static final int[][] ROOK_DIRECTIONS = {
+            {1,0}, //Up
+            {0,-1}, //Left
+            {-1,0}, //Down
+            {0,1} //Right
+    };
+
+    private static final int[][] KING_QUEEN_DIRECTIONS = {
+            {1,1}, //Up and right
+            {1,-1}, //Up and left
+            {-1,-1}, //Down and left
+            {-1,1}, //Down and right
+            {1,0}, //Up
+            {0,-1}, //Left
+            {-1,0}, //Down
+            {0,1} //Right
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return pieceColor == piece.pieceColor && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
@@ -64,22 +96,44 @@ public class ChessPiece {
         ChessPiece piece = board.getPiece(myPosition);
         List<ChessMove> moves = new ArrayList<>();
 
-        if (piece.getPieceType() == PieceType.BISHOP) {
-            for (int[] direction : BISHOP_DIRECTIONS) {
-                int row = myPosition.getRow();
-                int col = myPosition.getColumn();
+        int[][] directions = switch (type) {
+            case KING -> KING_QUEEN_DIRECTIONS;
+            case QUEEN -> KING_QUEEN_DIRECTIONS;
+            case BISHOP -> BISHOP_DIRECTIONS;
+            case ROOK -> ROOK_DIRECTIONS;
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        };
+        for (int[] direction : directions) {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
 
-                while (true) {
-                    row += direction[0];
-                    col += direction[1];
+            while (true) {
+                row += direction[0];
+                col += direction[1];
 
-                    if (row < 1 || row > 8 || col < 1 || col > 8) {
-                        break;
-                    }
-                    moves.add(new ChessMove(myPosition, new ChessPosition(row,col),null));
+                if (row < 1 || row > 8 || col < 1 || col > 8) {
+                    break;
                 }
+                moves.add(new ChessMove(myPosition, new ChessPosition(row,col),null));
             }
         }
+
+//        if (piece.getPieceType() == PieceType.BISHOP) {
+//            for (int[] direction : BISHOP_DIRECTIONS) {
+//                int row = myPosition.getRow();
+//                int col = myPosition.getColumn();
+//
+//                while (true) {
+//                    row += direction[0];
+//                    col += direction[1];
+//
+//                    if (row < 1 || row > 8 || col < 1 || col > 8) {
+//                        break;
+//                    }
+//                    moves.add(new ChessMove(myPosition, new ChessPosition(row,col),null));
+//                }
+//            }
+//        }
         return moves;
     }
 }
