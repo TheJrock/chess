c81e05fe27065f08ae0665648188246ca5a3faeb
Johnathan Goodlad <thejagoodlad@gmail.com>
2025-09-18 17:26:49 -0600

    Implemented resetBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -56,6 +56,32 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        clearBoard();
+
+        ChessPiece.PieceType[] backRank = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+
+        for (int column = 1; column < 8; column++) {
+            //Add White Pieces
+            addPiece(new ChessPosition(1, column), new ChessPiece(ChessGame.TeamColor.WHITE, backRank[column-1]));
+            addPiece(new ChessPosition(2, column), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            //Add Black Pieces
+            addPiece(new ChessPosition(8, column), new ChessPiece(ChessGame.TeamColor.BLACK, backRank[column-1]));
+            addPiece(new ChessPosition(7, column), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+    }
+
+    private void clearBoard() {
+        for (ChessPiece[] chessPieces : board) {
+            Arrays.fill(chessPieces, null);
+        }
     }
 }
