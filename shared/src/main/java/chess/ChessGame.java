abdf0e7e37c90a9943feab62d52acf795436dc28
Johnathan Goodlad <thejagoodlad@gmail.com>
2025-10-03 18:02:19 -0600

    worked on castling

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
+//import Math.abs;
 
 /**
  * For a class that can manage a chess game, making moves on a board
