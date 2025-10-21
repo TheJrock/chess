9cc794d32b2b32f640c0500e6a00f57a43f6f945
Johnathan Goodlad <thejagoodlad@gmail.com>
2025-10-21 09:20:56 -0600

    Small progress on Project 3

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,52 @@
 package server;
 
+import dataaccess.DataAccess;
 import io.javalin.*;
+import com.google.gson.Gson;
+import javax.naming.Context;
+import java.util.Map;
+import datamodel.*;
+import org.w3c.dom.CDATASection;
+import service.UserService;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
+    private final UserService userService;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        var dataAccess = new DataAccess();
+        userService = new UserService(dataAccess);
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+
+        server.delete("db", ctx -> result("{}"));
+        server.post("user", this::register);
 
         // Register your endpoints and exception handlers here.
 
     }
 
+    private void register(Context ctx) {
+        try {
+            var serializer = new Gson();
+            String reqJson = ctx.body();
+            var user = serializer.fromJson(reqJson, UserData.class);
+
+            var authData = userService.register(user);
+
+            ctx.result(serializer.toJson(authData));
+        } catch (Exception ex) {
+            var message = String.format("Error: %s", ex.getMessage());
+            ctx.status(403).result(message);
+        }
+    }
+
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
