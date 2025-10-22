52d5f904718773eba7dfe71ade40aa68a09e2b14
Johnathan Goodlad <thejagoodlad@gmail.com>
2025-10-22 17:07:04 -0600

    Wrote DataAccess Tests

--- server/src/tests/java/dataaccess/DataAccessTest.java
+++ server/src/tests/java/dataaccess/DataAccessTest.java
@@ -1,18 +1,31 @@
 package dataaccess;
 
+import datamodel.UserData;
 import org.junit.jupiter.api.Test;
 
 class DataAccessTest {
 
     @Test
     void clear() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        dataAccess.createUser(new UserData("username", "email", "password"));
+        dataAccess.clear();
+        assert dataAccess.getUser("username") == null;
     }
 
     @Test
     void createUser() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        dataAccess.createUser(new UserData("username", "email", "password"));
+        UserData user = dataAccess.getUser("username");
+        assert user.equals(new UserData("username", "email", "password"));
     }
 
     @Test
     void getUser() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        dataAccess.createUser(new UserData("username", "email", "password"));
+        UserData user = dataAccess.getUser("username");
+        assert user.equals(new UserData("username", "email", "password"));
     }
 }
